9b24ab98200a3bcbe15a888382f5580af27d05dd
nmelgar <nmelgar16013@gmail.com>
2025-01-11 21:07:55 -0600

    Initialize node.js project. Create server.

---
+++ api/index.js
@@ -0,0 +1,18 @@
+// Import the 'http' module
+const http = require('http');
+
+// Create a server
+const server = http.createServer((req, res) => {
+    // Set the response HTTP header
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    // Send a response
+    res.end('Nefi Melgar');
+});
+
+// Define a port
+const PORT = 3000;
+
+// Start the server
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
